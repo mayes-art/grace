c3385e27119536e13d80b3ce8b4942104f8f466e
Jun <mayes0art@gmail.com>
2024-10-11 16:35:40 +0800

    feat: 測試channel情境

--- controller/jun_controller.go
+++ controller/jun_controller.go
@@ -8,6 +8,30 @@ import (
 
 type JunController struct{}
 
+func shareComm(n int) []int {
+	var ord []int
+	ch := make(chan int)
+
+	for i := 0; i < n; i++ {
+		go func(ch chan<- int, order int) {
+			ch <- order
+		}(ch, i)
+	}
+
+	for i := range ch {
+		ord = append(ord, i)
+
+		if len(ord) == n {
+			break
+		}
+	}
+
+	close(ch)
+
+	return ord
+}
+
 func (j *JunController) GetReqHeader(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"message": "OK"})
+
+	c.JSON(http.StatusOK, gin.H{"message": "OK", "data": shareComm(5)})
 }
